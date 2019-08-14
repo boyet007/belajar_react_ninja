ea73816279f1d9ac80de71e4e390fbafe9a039cf
Philip Loa <boyet007@gmail.com>
2019-08-14 16:24:46 +0700

    14 - Output LIsts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Ninjas from './Ninjas'
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My First React app</h1>
-    <p>Welcome</p>
-    <Ninjas name="Ryu" age="25" belt="black"/>
-    <Ninjas name="Yoshi" age="26" belt="red" />
-    </div>
-  );
+export class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first Reacgt App!</h1>
+        <p>Welcome</p>
+        <Ninjas ninjas={this.state.ninjas} />
+        </div>
+    )
+  }
 }
 
-export default App;
+export default App
