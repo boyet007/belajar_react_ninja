ef9205d23ffb90abd630afa4728aa053850ba2a2
Philip Loa <boyet007@gmail.com>
2019-08-16 13:13:08 +0700

    16 - Conditional Output

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
 
 const Ninjas = ({ ninjas }) => {
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className="ninja" key={ninja.id} >
-                   <div>Name: { ninja.name}</div>
-                   <div>Age: { ninja.age}</div>
-                   <div>Belt: { ninja.belt}</div>
-                </div>
-            )
-        })
+        // const ninjaList = ninjas.map(ninja => {
+        //     if (ninja.age > 20) {
+        //         return (
+        //             <div className="ninja" key={ninja.id} >
+        //             <div>Name: { ninja.name}</div>
+        //             <div>Age: { ninja.age}</div>
+        //             <div>Belt: { ninja.belt}</div>
+        //             </div>
+        //         )
+        //     } else {
+        //         return null
+        //     }
+        // })
+
+        // const ninjaList = ninjas.map( ninja => {
+        //     return ninja.age > 20 ? (
+        //         <div className="ninja" key={ninja.id} >
+        //         <div>Name: { ninja.name}</div>
+        //         <div>Age: { ninja.age}</div>
+        //         <div>Belt: { ninja.belt}</div>
+        //         </div>
+        //     ) : null
+        // })
+
         console.log(ninjas)
         return(
             <div className="ninja-list">
-                { ninjaList}
+                {  
+                    ninjas.map( ninja => {
+                       return ninja.age > 20 ? (
+                           <div className="ninja" key={ninja.id} >
+                           <div>Name: { ninja.name}</div>
+                           <div>Age: { ninja.age}</div>
+                           <div>Belt: { ninja.belt}</div>
+                           </div>
+                       ) : null
+                    }) }
             </div>
         )
 }
