6d4c2bb0ac3914b035b294f1523a8b7d88670879
Philip Loa <boyet007@gmail.com>
2019-08-19 12:13:42 +0700

    24 - Todo App (part 2)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Todos from './Todos'
+import AddTodo from './AddTodo'
 
 
 export default class App extends Component {
@@ -20,12 +21,22 @@ export default class App extends Component {
       todos
     })
   }
+
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+
+  }
   
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todos</h1> 
         <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } /> 
+        <AddTodo addTodo={this.addTodo} />
       </div>
     )
   }
