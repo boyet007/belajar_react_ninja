86fc03553feb2c480c921daf65de56246142bad3
Philip Loa <boyet007@gmail.com>
2019-08-20 16:22:44 +0700

    40 - Mapping State to Props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Pokeball from '../pokeball.png'
 
-export class Home extends Component {
-    
-    state = {
-        posts: []
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts/')
-            .then(res => {
-                this.setState({
-                    posts:res.data.slice(0, 10)
-                })
-            })
-    }
-    
+export class Home extends Component {   
     render() {
-        const { posts } = this.state
+        console.log(this.props)
+        const { posts } = this.props
         const postList = posts.length ? (
               posts.map(post => {
                   return(
                     <div className="post card" key={ post.id }>
+                        <img src={Pokeball} alt="A Pokeball" />
                         <div className="card-content">
                             <Link to={'/' + post.id}>
                             <span className="card-title">{ post.title }</span>
@@ -45,4 +34,10 @@ export class Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = ( state ) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home)
