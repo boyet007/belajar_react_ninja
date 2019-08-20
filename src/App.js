ae0b6232711d7f3f08d8f9266440d9a0c236b22c
Philip Loa <boyet007@gmail.com>
2019-08-20 12:49:55 +0700

    30 - Route Parameters (part 1)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 export class App extends Component {
     render() {
@@ -14,7 +15,7 @@ export class App extends Component {
                     <Route exact path="/" component={Home} />
                     <Route path="/about" component={About} />
                     <Route path="/contact" component={Contact} />
-
+                    <Route path="/posts/:post_id" component={Post} />
                 </div>
             </BrowserRouter>
 
