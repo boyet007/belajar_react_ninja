875b94bb5a43b3f951543dd45ff846ac1ee12e37
boyet <boyet007@gmail.com>
2019-09-16 00:41:28 +0700

    riset

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react'
-import Todos from './Todos'
-
+import React, { Component } from "react";
+import Todos from "./Todos";
+import {Card, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
-  
   state = {
     todos: [
-      { id: 1, content: 'buy some milk'},
-      { id: 2, content: 'play mario kart'}
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" }
     ]
-  }
+  };
 
-  deleteTodo = (id) => {
+  deleteTodo = id => {
     const todos = this.state.todos.filter(todo => {
-      return todo.id !== id
-    })
+      return todo.id !== id;
+    });
 
     this.setState({
       todos
-    })
-  }
-  
+    });
+  };
+
   render() {
     return (
-      <div className="todo-app container">
-        <h1 className="center blue-text">Todos</h1> 
-        <Todos todos={ this.state.todos } deleteTodo={ this.deleteTodo } /> 
-      </div>
-    )
+      // <div className="todo-app container">
+      //   <h1 className="center blue-text">Todos</h1>
+      //   <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+      // </div>
+      
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>)
   }
 }
