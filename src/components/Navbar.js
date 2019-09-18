71601893d98c8bb7b8c2ec35a7f88783341882d9
Philip <boyet007@gmail.com>
2019-09-19 00:11:01 +0700

    26 - Link and NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="na-wrapper red darken-3">
             <div className="container">
                 <a href="/" className="brand-logo">Poke's Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><NavLink exact to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
     ) 
 }
+
+//NavLink supaya linknya bisa di set active untuk di style
 
 export default Navbar
