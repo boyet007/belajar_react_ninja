78a99b4a79f70fb2f5fcdccf9707df51319c9f49
Philip Loa <boyet007@gmail.com>
2019-09-18 16:53:15 +0700

    nyoba routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="nav-wrapper red darken-1">
+            <div className="container">
+                <a href="" className="brand-logo">Poke's Times</a>
+                <ul>
+                    <li><a></a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
