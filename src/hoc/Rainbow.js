17583999acc8c7bd6dbb0129bf10dbac3a819171
Philip Loa <boyet007@gmail.com>
2019-09-19 11:08:35 +0700

    30 - Route Parameters 1

--- src/hoc/Rainbow.js
+++ src/hoc/Rainbow.js
@@ -1,7 +1,6 @@
 import React  from 'react'
 
 const Rainbow = (WrappedComponent) => {
-    console.log(WrappedComponent)
     const colours = ['yellow', 'pink', 'orange', 'blue', 'green']
     const randomColour = colours[Math.floor(Math.random() * 5)]
     const className = randomColour+ '-text'
