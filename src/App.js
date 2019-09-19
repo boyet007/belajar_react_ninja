be7acb1b1b605e47a4167107b1b994277f2a0d3b
Philip Loa <boyet007@gmail.com>
2019-09-19 13:25:30 +0700

    rombak lagi

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
-import React, { Component } from "react";
-import Todos from "./Todos";
-import {Card, Button} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default class App extends Component {
-  state = {
-    todos: [
-      { id: 1, content: "buy some milk" },
-      { id: 2, content: "play mario kart" }
-    ]
-  };
-
-  deleteTodo = id => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.id !== id;
-    });
-
-    this.setState({
-      todos
-    });
-  };
-
-  render() {
-    return (
-      // <div className="todo-app container">
-      //   <h1 className="center blue-text">Todos</h1>
-      //   <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
-      // </div>
-      
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>)
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
